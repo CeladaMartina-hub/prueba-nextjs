bbd75e21cf6141350e3ff23c17d7440cf3579d6d
Martina <mcelada@quadiontech.com>
2026-07-31 20:08:59 -0300

    creacion de nuevas paginas

--- auth.config.ts
+++ auth.config.ts
@@ -8,13 +8,12 @@ export const authConfig = {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
+
       if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL("/dashboard", nextUrl));
+        return isLoggedIn; // solo entra si está logueado, si no, a /login
       }
-      return true;
+
+      return true; // todas las demás rutas son públicas siempre
     },
   },
   providers: [], // Add providers with an empty array for now
